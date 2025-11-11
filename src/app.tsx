2570b749a932c971b0bde3e0a7db4bf8306faa3b
bando9 <bandomega1@gmail.com>
2025-11-11 12:33:18 +0700

    chore: simplify search icon;

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,8 @@
-import searchIcon from "/search.svg";
 import { Summary } from "./components/summary";
 import { Footer } from "./components/footer";
 import { ListTask } from "./components/list-task";
 import "./index.css";
-import { dataTasks } from "./lib/storage";
 import { SearchForm } from "./components/search-form";
-
-function sortByPriority() {
-  const tasks = dataTasks;
-  tasks.sort((a, b) => a.status.id - b.status.id);
-}
-sortByPriority();
 
 function App() {
   return (
@@ -23,7 +15,7 @@ function App() {
         <section className=" mt-5 flex justify-center">
           <div>
             <h1 className="text-2xl font-semibold mb-4">Today</h1>
-            <SearchForm searchIcon={searchIcon} />
+            <SearchForm />
             <ListTask />
           </div>
         </section>
