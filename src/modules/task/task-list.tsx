184c6e59a43731ae4a5c604c80c4367c87c6eacc
bando9 <bandomega1@gmail.com>
2025-11-18 00:02:37 +0700

    feat(wip): handle add task;

--- src/modules/task/task-list.tsx
+++ src/modules/task/task-list.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { initialDataTasks } from "@/lib/storage";
 import { TaskItem } from "./task-item";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 
 export function TaskList() {
   const [tasks, setTasks] = useState(initialDataTasks);
@@ -10,17 +13,43 @@ export function TaskList() {
     setTasks(updatedTasks);
   }
 
+  function handleCreateTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const title = formData.get("title");
+
+    const newId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+
+    const newTask = {
+      id: newId,
+      title,
+      isDone,
+    };
+
+    const updateTasks = [...tasks, newTask];
+    console.log(title);
+  }
+
   return (
-    <ul>
-      {tasks.map((task) => {
-        return (
-          <TaskItem
-            key={task.id}
-            task={task}
-            handleDelete={() => handleDelete(task.id)}
-          />
-        );
-      })}
-    </ul>
+    <section className="space-y-8">
+      <form method="post" className="space-y-2" onSubmit={handleCreateTask}>
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" name="title" id="title" />
+        <Button>Create Task</Button>
+      </form>
+
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <TaskItem
+              key={task.id}
+              task={task}
+              handleDelete={() => handleDelete(task.id)}
+            />
+          );
+        })}
+      </ul>
+    </section>
   );
 }
