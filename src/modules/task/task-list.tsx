adc145aee7c6c8ceafe4ea843c10769ea4fb4a91
bando9 <bandomega1@gmail.com>
2025-11-21 06:20:04 +0700

    feat: description & select option form task

--- src/modules/task/task-list.tsx
+++ src/modules/task/task-list.tsx
@@ -5,10 +5,18 @@ import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { TaskSchema, type Task, type Tasks } from "@/modules/task/schema";
-import { CalendarForm } from "./calender-form";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export function TaskList() {
   const [tasks, setTasks] = useState(initialDataTasks);
+  const [status, setStatus] = useState("");
 
   function handleDelete(id: number) {
     const updatedTasks = tasks.filter((task) => task.id !== id);
@@ -22,11 +30,14 @@ export function TaskList() {
 
     const newId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
 
+    const statusName = formData.get("status");
+    console.log(statusName);
+
     const newTask: Task = {
       id: newId,
       title: formData.get("title")?.toString().trim() || "",
+      description: formData.get("description")?.toString().trim() || "",
       status: { id: 2, name: "todo" },
-      description: "-".trim(),
       createdAt: new Date(),
       updatedAt: new Date(),
     };
@@ -88,7 +99,25 @@ export function TaskList() {
       <form method="post" className="space-y-2" onSubmit={handleCreateTask}>
         <Label htmlFor="title">Title Task</Label>
         <Input type="text" name="title" id="title" required />
-        <CalendarForm />
+
+        <Label htmlFor="description">Description Task</Label>
+        <Input type="text" name="description" id="description" required />
+
+        <Select onValueChange={setStatus}>
+          <SelectTrigger className="w-36">
+            <SelectValue placeholder="Select Status" />
+          </SelectTrigger>
+          <SelectContent id="status">
+            <SelectGroup>
+              {/* <SelectLabel>Status</SelectLabel> */}
+              <SelectItem value="todo">Todo</SelectItem>
+              <SelectItem value="in-progress">In-Progress</SelectItem>
+              <SelectItem value="backlog">Backlog</SelectItem>
+              <SelectItem value="done">Done</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <input type="hidden" name="status" value={status} />
 
         <Button>Create Task</Button>
       </form>
