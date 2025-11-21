77bae9ec50136cf81e2ce0f1e3b3e08dd57fea83
bando9 <bandomega1@gmail.com>
2025-11-21 21:06:35 +0700

    refactor: refine navbar styling

--- src/modules/board/board-view.tsx
+++ src/modules/board/board-view.tsx
@@ -1,10 +1,19 @@
+import { Footer } from "@/components/footer";
 import { NavBar } from "@/modules/navbar";
 
 export function BoardView() {
   return (
     <>
-      <NavBar />
-      <h1>Hello from Board View</h1>
+      <main className="w-full min-h-screen p-10 overflow-hidden transition-all duration-300 mx-auto overflow-y-scroll">
+        <section className="mt-5 w-3/4 mx-auto">
+          <NavBar />
+        </section>
+        <section className="mt-14 w-3/4 mx-auto">
+          <h1>Hello from Board View</h1>
+        </section>
+
+        <Footer />
+      </main>
     </>
   );
 }
